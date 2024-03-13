fa9365ba07cca5c45719f19010d9352bd4f34fca
CoC-Fire <satcatkat@gmail.com>
2024-03-12 22:20:08 -0700

    began making general file structure (2)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,12 @@
 # CustomCommands.utils.__init__.py
+"""
+Utility functions to be used in other parts of the system
+"""
 from __future__ import annotations
+
+from .to_dict import to_dict
+
+
+__all__ = [
+    'to_dict',
+]
